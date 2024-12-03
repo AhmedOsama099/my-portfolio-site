ea44ceb9f461b201ed4b9fb7c5480df2acdf04da
AhmedOsama099 <ahmedeng099@gmail.com>
2024-12-03 23:49:20 +0200

    feat(home): Add comments

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* Image Container */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 relative ">
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
         <Image
           src={"/hero.png"}
           alt="myProfile"
@@ -14,15 +14,18 @@ export default function Home() {
       </div>
       {/* Text  Container */}
       <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/* Title */}
         <h1 className="text-4xl md:text-6xl font-bold">
           Crafting Digital Experience, Designing Tomorrow.
         </h1>
+        {/* Desc */}
         <p className="md:text-xl">
           Welcome to my digital canvas, Where Innovation and creativity
           converge. with a keen eye for aesthetics and a mystery of code, my
           portfolio show case a diverse collection of projects that reflect my
           commitment to excellence.
         </p>
+        {/* Buttons */}
         <div className="w-full flex gap-4">
           <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
             View My Work
