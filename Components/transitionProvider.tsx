24c28d0e8e0cde8b7f5e23c94573ed319509d939
AhmedOsama099 <ahmedeng099@gmail.com>
2025-01-10 22:46:34 +0200

    style(TransitionProvider): Adjust container style to enable scroll and be sized to min size

--- Components/transitionProvider.tsx
+++ Components/transitionProvider.tsx
@@ -16,7 +16,7 @@ const TransitionProvider: FC<{
     <AnimatePresence mode="wait">
       <div
         key={pathName}
-        className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100 "
+        className="w-screen h-screen overflow-y-auto bg-gradient-to-b from-blue-100 to-red-100 min-w-56"
       >
         <motion.div
           className="w-screen h-screen fixed bg-black rounded-b-[100px] z-40"
