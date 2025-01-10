4c609bb1d98921027a84ceb5286f22e2dfb9e86c
AhmedOsama099 <ahmedeng099@gmail.com>
2025-01-10 22:05:23 +0200

    refactor(Const): create constants folder and unify general padding in it.

---
+++ app/const/index.tsx
@@ -0,0 +1,1 @@
+export const globalPX = "sm:px-8 md:px-12 lg:px-20 xl:px-48";
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import HomeDescription from "@/Components/home/HomeDescription";
 import HomeImage from "@/Components/home/HomeImage";
 import MotionPageWrapper from "@/Components/motionPageWrapper";
+import { globalPX } from "./const";
 
 export default function Home() {
   return (
     <MotionPageWrapper>
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className={`h-full flex flex-col lg:flex-row px-4 ${globalPX}`}>
         {/* Image Container */}
         <HomeImage />
         {/* Text  Container */}
