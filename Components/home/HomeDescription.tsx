e990965f03fccfd965091e54ec93580db94e146b
AhmedOsama099 <ahmedeng099@gmail.com>
2025-01-10 21:59:56 +0200

    refactor(Home): Separate Components

---
+++ Components/home/HomeDescription.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const HomeDescription = () => {
+  return (
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+      {/* Title */}
+      <h1 className="text-4xl md:text-6xl font-bold">
+        Crafting Digital Experience, Designing Tomorrow.
+      </h1>
+      {/* Desc */}
+      <p className="md:text-xl">
+        Welcome to my digital canvas, Where Innovation and creativity converge.
+        with a keen eye for aesthetics and a mystery of code, my portfolio show
+        case a diverse collection of projects that reflect my commitment to
+        excellence.
+      </p>
+      {/* Buttons */}
+      <div className="w-full flex gap-4">
+        <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+          View My Work
+        </button>
+        <button className="p-4 rounded-lg ring-1 ring-black ">
+          Contact Me
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeDescription;
---
+++ Components/home/HomeImage.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const HomeImage = () => {
+  return (
+    <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+      <Image
+        src={"/hero.png"}
+        alt="myProfile"
+        fill
+        className="object-contain"
+        priority
+      />
+    </div>
+  );
+};
+
+export default HomeImage;
