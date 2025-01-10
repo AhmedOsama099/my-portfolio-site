82198e3af34e2c89d7b49c76054985d7afd79e80
AhmedOsama099 <ahmedeng099@gmail.com>
2025-01-10 22:47:17 +0200

    style(HomeDescription): Adjust text variations between start and center due to different container stages

--- Components/home/HomeDescription.tsx
+++ Components/home/HomeDescription.tsx
@@ -2,24 +2,24 @@ import React from "react";
 
 const HomeDescription = () => {
   return (
-    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 justify-normal lg:justify-center lg:mt-0 mt-4 ">
       {/* Title */}
-      <h1 className="text-4xl md:text-6xl font-bold">
+      <h1 className="text-4xl md:text-6xl font-bold lg:text-start text-center">
         Crafting Digital Experience, Designing Tomorrow.
       </h1>
       {/* Desc */}
-      <p className="md:text-xl">
+      <p className="md:text-xl lg:text-start text-center">
         Welcome to my digital canvas, Where Innovation and creativity converge.
         with a keen eye for aesthetics and a mystery of code, my portfolio show
         case a diverse collection of projects that reflect my commitment to
         excellence.
       </p>
       {/* Buttons */}
-      <div className="w-full flex gap-4">
-        <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+      <div className="w-full flex flex-col sm:flex-row gap-4  ">
+        <button className="p-4 flex-1  rounded-lg ring-1 ring-black bg-black text-white">
           View My Work
         </button>
-        <button className="p-4 rounded-lg ring-1 ring-black ">
+        <button className="p-4 flex-1  rounded-lg ring-1 ring-black sm:mb-0 mb-4 ">
           Contact Me
         </button>
       </div>
