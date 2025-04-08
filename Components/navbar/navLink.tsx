46b3be678a8eda184c93fcd345d19a9439a97191
AhmedOsama099 <ahmedeng099@gmail.com>
2025-04-09 01:57:52 +0200

    style(navbar): improve styling and active state animation for nav links
    Enhance the visual feedback for active navigation links by adding animated elements and updating the color scheme. Additionally, align items vertically in the desktop navigation bar for better consistency.

--- Components/navbar/navLink.tsx
+++ Components/navbar/navLink.tsx
@@ -6,13 +6,28 @@ import { NavLinkProps } from "@/types/navbar";
 const NavLink: React.FC<NavLinkProps> = ({ link }) => {
   const pathName = usePathname();
 
-  const isActive = pathName === link.url ? "bg-black text-white" : "";
+  const isActive =
+    pathName === link.url
+      ? "bg-[#FCE0A2] text-[#565FA1] font-medium outline-2  p-2 outline-dashed  hover:outline-dotted transition-all duration-300"
+      : "";
   return (
     <Link
-      className={`rounded p-1 ${isActive}`}
+      className={`rounded p-1 ${isActive} relative`}
       key={link.title}
       href={link.url}
     >
+      {isActive && (
+        <div className="z-20">
+          <div className="absolute w-2 h-2 bg-[#FFB800] rounded-full -top-1 -left-1 animate-pulse"></div>
+          <div className="absolute w-3 h-3 bg-[#EE3E54] rounded-full -bottom-1 -right-1 animate-pulse"></div>
+          <div className="absolute w-1.5 h-1.5 bg-[#FFB800] rounded-full top-1/2 -left-2 animate-pulse"></div>
+          <div className="absolute w-1.5 h-1.5 bg-[#EE3E54] rounded-full top-1/2 -right-2 animate-pulse"></div>
+          <div className="absolute w-1.5 h-1.5 bg-white rounded-full -top-2 left-3 animate-pulse"></div>
+          <div className="absolute w-1.5 h-1.5 bg-white rounded-full bottom-0 left-1/2 animate-pulse"></div>
+          <div className="absolute w-1.5 h-1.5 bg-[#8889B9] rounded-full -top-0.5 right-2 animate-pulse"></div>
+          <div className="absolute w-1 h-1 bg-[#8889B9] rounded-full -bottom-1.5 left-2 animate-pulse"></div>
+        </div>
+      )}
       {link.title}
     </Link>
   );
