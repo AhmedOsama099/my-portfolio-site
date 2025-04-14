1a5a43b64349b0f139721e1f1589fcfd04502c49
AhmedOsama099 <ahmedeng099@gmail.com>
2025-04-14 11:41:11 +0200

    refactor(navbar): remove unnecessary "use client" directive and adjust logo dimensions
    The "use client" directive was removed from SocialMediaLinks.tsx as it was unnecessary. Additionally, the logo dimensions in NavLogo.tsx were explicitly set to improve consistency and control over the image rendering.

--- Components/navbar/NavLogo.tsx
+++ Components/navbar/NavLogo.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const NavLogo: React.FC = () => (
   <div className="w-1/3 flex-col justify-center flex min-[980px]:items-center items-start">
     <Link href="/">
-      <Image src={"/images/logo.svg"} alt="logo" sizes={"100%"} />
+      <Image src={"/images/logo.svg"} alt="logo" width={150} height={150} />
     </Link>
   </div>
 );
