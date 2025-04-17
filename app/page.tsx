e52d00f2c1c770b213242b5f406553a2d5cfe447
AhmedOsama099 <ahmedeng099@gmail.com>
2025-04-17 02:44:36 +0200

    style(page): update div height classes for better responsiveness
    Adjust the height classes of the div to ensure better responsiveness across different screen sizes. Specifically, added `md:min-h-full` and `min-h-screen` to handle height on medium and smaller screens.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ import { socialMedia } from "@/data/navbar";
 export default function Home() {
   return (
     <MotionPageWrapper>
-      <div className={`h-full flex flex-col lg:flex-row px-4 ${globalPX}`}>
+      <div
+        className={`h-full md:min-h-full min-h-screen  flex flex-col lg:flex-row px-4 ${globalPX}`}
+      >
         {/* Image Container */}
         <HomeImage />
 
