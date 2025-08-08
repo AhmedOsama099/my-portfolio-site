b8329b6e168197db467fc430c00f093b949ae75a
AhmedOsama099 <ahmedeng099@gmail.com>
2025-08-08 16:16:41 +0300

    style(tech-blog): update card styling with new background and badge
    Add "Soon" badge to technology cards and change background color to improve visual hierarchy and indicate upcoming content

--- app/tech-blog/page.tsx
+++ app/tech-blog/page.tsx
@@ -14,13 +14,18 @@ const TechBlogPage = () => {
       {technologies.map((tech, index) => (
         <div
           key={index}
-          className="border border-gray-300 py-3 px-5 rounded-3xl bg-gray-950 shadow-sm size-48 flex items-center justify-center
+          className="relative border border-gray-300 py-3 px-5 rounded-3xl bg-[#302d2d] shadow-sm size-48 flex items-center justify-center
           cursor-pointer
           hover:scale-110
           transition-all
-          duration-300
-          "
+          duration-300"
         >
+          <span
+            className="absolute top-0 right-0 bg-yellow-500 text-white text-xs font-bold py-1 px-2 rounded-full"
+            style={{ transform: "translate(50%, -50%) rotate(10deg)" }}
+          >
+            Soon
+          </span>
           <Image src={tech.imageURL} alt={tech.alt} width={150} height={150} />
         </div>
       ))}
