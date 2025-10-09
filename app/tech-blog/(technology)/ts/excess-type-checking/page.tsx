1432e13669734877149c0fa15c5e3236ac49e69d
AhmedOsama099 <ahmedeng099@gmail.com>
2025-10-09 23:10:38 +0300

    refactor(excess-type-checking): comment out translation logic for clarity
    - Comment out the translation import and related logic in the Excess Type Checking page for improved readability and to prevent unnecessary execution during development.

--- app/tech-blog/(technology)/ts/excess-type-checking/page.tsx
+++ app/tech-blog/(technology)/ts/excess-type-checking/page.tsx
@@ -1,9 +1,9 @@
 import HeaderTitle from "@/Components/techBlog/DocsUtils/HeaderTitle";
 import PageHeader from "@/Components/techBlog/DocsUtils/PageHeaderWrapper";
-import { getServerTranslation } from "@/utils/getServerTranslation";
+// import { getServerTranslation } from "@/utils/getServerTranslation";
 
 export default async function ExcessTypeCheckingPage() {
-  const { t, lang } = await getServerTranslation("excess-type-checking");
+  // const { t, lang } = await getServerTranslation("excess-type-checking");
 
   return (
     <>
